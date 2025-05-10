1f2d442dd96924ea1a9f74d457d7feb0f6801e2b
Jake Samuels <samuels242@googlemail.com>
2025-05-10 22:04:55 +0100

    Import recipe search results

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
-import Hero from "@/components/Hero";
+import Hero from "@/components/homepage/Hero";
+import Recipes from "@/components/homepage/Recipes";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <Recipes />
     </div>
   );
 };
