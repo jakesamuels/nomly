677e8e8bc81024147697a5378c4f37b0f7929497
Jake Samuels <samuels242@googlemail.com>
2025-05-10 19:22:01 +0100

    Use RecipeProvider

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { RecipeProvider } from "@/context/RecipeContext";
 
 export const metadata = {
   title: "Nomly",
@@ -8,7 +9,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <RecipeProvider>{children}</RecipeProvider>
+      </body>
     </html>
   );
 }
