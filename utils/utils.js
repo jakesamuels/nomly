11b3428758a1ef985b7e30110b3fe54bad273672
Jake Samuels <samuels242@googlemail.com>
2025-05-10 18:19:21 +0100

    Create recipe search util function

---
+++ utils/utils.js
@@ -0,0 +1,16 @@
+// Spoonacular API
+const apiKey = process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY;
+
+export const searchRecipesBytTitle = async (query) => {
+  try {
+    const data = await fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&titleMatch=${query}&number=100`
+    );
+
+    const recipeData = await data.json();
+
+    return recipeData;
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+};
