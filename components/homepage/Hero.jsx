5b32980e7480396535763564da3f2dba857db812
Jake Samuels <samuels242@googlemail.com>
2025-05-10 22:05:21 +0100

    Render search results

---
+++ components/homepage/Hero.jsx
@@ -0,0 +1,17 @@
+import { SearchRecipes } from "../SearchRecipes";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="text-2xl">Unlock Your Perfect Plate with Nomly</h1>
+      <p>
+        Say goodbye to recipe overwhelm. Nomly learns your tastes, dietary
+        needs, and even your pantry staples to deliver delicious dishes crafted
+        just for you.
+      </p>
+      <SearchRecipes />
+    </div>
+  );
+};
+
+export default Hero;
