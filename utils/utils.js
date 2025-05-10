8277e1d191ce83dde7547f620f3ad906aa2dbf34
Jake Samuels <samuels242@googlemail.com>
2025-05-10 18:47:04 +0100

    Remove * styles

--- utils/utils.js
+++ utils/utils.js
@@ -4,7 +4,7 @@ const apiKey = process.env.NEXT_PUBLIC_SPOONACULAR_API_KEY;
 export const searchRecipesBytTitle = async (query) => {
   try {
     const data = await fetch(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&titleMatch=${query}&number=100`
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&titleMatch=${query}&number=9`
     );
 
     const recipeData = await data.json();
