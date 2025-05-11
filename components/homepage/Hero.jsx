2e33d44e47c979764158e1d9deb968bdf9ddc016
Jake Samuels <samuels242@googlemail.com>
2025-05-11 18:05:44 +0100

    Comment out searchbar

--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -2,15 +2,15 @@ import { SearchRecipes } from "../SearchRecipes";
 
 const Hero = () => {
   return (
-    <div>
+    <section>
       <h1 className="text-2xl">Unlock Your Perfect Plate with Nomly</h1>
       <p>
         Say goodbye to recipe overwhelm. Nomly learns your tastes, dietary
         needs, and even your pantry staples to deliver delicious dishes crafted
         just for you.
       </p>
-      <SearchRecipes />
-    </div>
+      {/* <SearchRecipes /> */}
+    </section>
   );
 };
 
